02c41722cbd5e9d39cba1c9e6ef24118093b7585
Reece H. Dunn <msclrhd@gmail.com>
2022-12-17 17:16:30 +0000

    Add a valueOutOfRange helper for reporting errors.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/Serialization.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/Serialization.kt
@@ -31,3 +31,17 @@ val JsonElement.kindType: String
 fun unsupportedKindType(json: JsonElement): Nothing {
     throw IllegalArgumentException("Unsupported kind type '${json.kindType}'")
 }
+
+/**
+ * A helper function for reporting numeric values outside supported bounds.
+ */
+fun <T> valueOutOfRange(value: T): Nothing {
+    throw IllegalArgumentException("The value '$value' is out of range")
+}
+
+/**
+ * A helper function for reporting numeric values outside supported bounds.
+ */
+fun valueOutOfRange(json: JsonPrimitive): Nothing {
+    throw IllegalArgumentException("The value '${json.content}' is out of range")
+}
