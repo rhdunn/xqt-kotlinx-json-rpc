d5c619475298a15a26d94b50a2a88517bdfe9a71
Reece H. Dunn <msclrhd@gmail.com>
2022-12-17 15:14:31 +0000

    Add a JsonSerialization interface.

---
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/JsonSerialization.kt
@@ -0,0 +1,19 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.serialization
+
+import kotlinx.serialization.json.JsonElement
+
+/**
+ * Defines serialization for a data type or object.
+ */
+interface JsonSerialization<T> {
+    /**
+     * Serialize the data type or object `value` to JSON.
+     */
+    fun serialize(value: T): JsonElement
+
+    /**
+     * Deserialize the data type or object from the `json` element.
+     */
+    fun deserialize(json: JsonElement): T
+}
