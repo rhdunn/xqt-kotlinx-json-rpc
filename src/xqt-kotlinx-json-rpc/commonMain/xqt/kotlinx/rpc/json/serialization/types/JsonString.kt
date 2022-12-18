9e69f6bb7f883b90a54f0f19c0956875a760682d
Reece H. Dunn <msclrhd@gmail.com>
2022-12-18 14:22:33 +0000

    Define a String JSON serializer.

---
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/JsonString.kt
@@ -0,0 +1,23 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.serialization.types
+
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.rpc.json.serialization.JsonSerialization
+import xqt.kotlinx.rpc.json.serialization.kindType
+import xqt.kotlinx.rpc.json.serialization.unsupportedKindType
+
+/**
+ * Defines a string.
+ */
+object JsonString : JsonSerialization<String> {
+    override fun serialize(value: String): JsonElement = JsonPrimitive(value)
+
+    override fun deserialize(json: JsonElement): String = when (json) {
+        !is JsonPrimitive -> unsupportedKindType(json)
+        else -> when (json.kindType) {
+            "string" -> json.content
+            else -> unsupportedKindType(json)
+        }
+    }
+}
---
+++ src/xqt-kotlinx-json-rpc/commonTest/xqt/kotlinx/rpc/json/test/serialization/types/JsonString.kt
@@ -0,0 +1,57 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.test.serialization.types
+
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+import xqt.kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+@DisplayName("The JSON String type")
+class TheJsonStringType {
+    @Test
+    @DisplayName("can serialize to JSON")
+    fun can_serialize_to_json() {
+        assertEquals("\"lorem\"", JsonString.serialize("lorem").toString())
+        assertEquals("\"1234\"", JsonString.serialize("1234").toString())
+    }
+
+    @Test
+    @DisplayName("can deserialize from JSON")
+    fun can_deserialize_from_json() {
+        assertEquals("lorem", JsonString.deserialize(JsonPrimitive("lorem")))
+        assertEquals("1234", JsonString.deserialize(JsonPrimitive("1234")))
+    }
+
+    @Test
+    @DisplayName("throws an error if the kind type is not supported")
+    fun throws_an_error_if_the_kind_type_is_not_supported() {
+        val e1 = assertFails { JsonString.deserialize(JsonObject(mapOf())) }
+        assertEquals(IllegalArgumentException::class, e1::class)
+        assertEquals("Unsupported kind type 'object'", e1.message)
+
+        val e2 = assertFails { JsonString.deserialize(JsonArray(arrayListOf())) }
+        assertEquals(IllegalArgumentException::class, e2::class)
+        assertEquals("Unsupported kind type 'array'", e2.message)
+
+        val e3 = assertFails { JsonString.deserialize(JsonNull) }
+        assertEquals(IllegalArgumentException::class, e3::class)
+        assertEquals("Unsupported kind type 'null'", e3.message)
+
+        val e4 = assertFails { JsonString.deserialize(JsonPrimitive(true)) }
+        assertEquals(IllegalArgumentException::class, e4::class)
+        assertEquals("Unsupported kind type 'boolean'", e4.message)
+
+        val e5 = assertFails { JsonString.deserialize(JsonPrimitive(1)) }
+        assertEquals(IllegalArgumentException::class, e5::class)
+        assertEquals("Unsupported kind type 'integer'", e5.message)
+
+        val e6 = assertFails { JsonString.deserialize(JsonPrimitive(1.2)) }
+        assertEquals(IllegalArgumentException::class, e6::class)
+        assertEquals("Unsupported kind type 'decimal'", e6.message)
+    }
+}
