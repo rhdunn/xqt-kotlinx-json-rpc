10aa267b888ff2c9fa26792770e0f58132d788f9
Reece H. Dunn <msclrhd@gmail.com>
2022-12-29 17:07:49 +0000

    Add a JsonObjectBuilder.putOptional extension method.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/Objects.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/Objects.kt
@@ -71,6 +71,31 @@ fun <T> JsonObjectBuilder.putArray(key: String, value: List<T>, serializer: Json
 }
 
 /**
+ * Serialize the data type or object to the JSON element.
+ *
+ * If the value is null, this will not add it to the JSON element.
+ *
+ * @param key the name of the key to serialize to.
+ * @param value the content of the array to serialize to.
+ */
+fun JsonObjectBuilder.putOptional(key: String, value: JsonElement?) {
+    value?.let { put(key, it) }
+}
+
+/**
+ * Serialize the data type or object to the JSON element.
+ *
+ * If the value is null, this will not add it to the JSON element.
+ *
+ * @param key the name of the key to serialize to.
+ * @param value the content of the array to serialize to.
+ * @param serializer how to serialize the JSON element value.
+ */
+fun <T> JsonObjectBuilder.putOptional(key: String, value: T?, serializer: JsonSerialization<T>) {
+    value?.let { put(key, serializer.serialize(it)) }
+}
+
+/**
  * Serialize the array of data types to the JSON element.
  *
  * If the list is empty, this will not add the array to the JSON element.
