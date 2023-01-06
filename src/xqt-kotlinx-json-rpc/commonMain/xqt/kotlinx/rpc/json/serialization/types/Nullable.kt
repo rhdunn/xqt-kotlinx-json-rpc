99fcd2913d50ac81d7ec1c13e71cb0b92e666f57
Reece H. Dunn <msclrhd@gmail.com>
2023-01-06 14:28:21 +0000

    Remove the now redundant JsonIntOrNull serializer.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/Nullable.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/Nullable.kt
@@ -7,25 +7,6 @@ import kotlinx.serialization.json.JsonPrimitive
 import xqt.kotlinx.rpc.json.serialization.*
 
 /**
- * Defines a nullable integer number in the range of -2^31 to 2^31 - 1.
- */
-object JsonIntOrNull : JsonSerialization<Int?> {
-    override fun serializeToJson(value: Int?): JsonElement = when (value) {
-        null -> JsonNull
-        else -> JsonPrimitive(value)
-    }
-
-    override fun deserialize(json: JsonElement): Int? = when (json) {
-        !is JsonPrimitive -> unsupportedKindType(json)
-        else -> when (json.kindType) {
-            KindType.Null -> null
-            KindType.Integer -> json.content.toIntOrNull() ?: valueOutOfRange(json)
-            else -> unsupportedKindType(json)
-        }
-    }
-}
-
-/**
  * Defines a nullable integer|string union type.
  */
 object JsonIntStringOrNull : JsonSerialization<JsonIntOrString?> {
