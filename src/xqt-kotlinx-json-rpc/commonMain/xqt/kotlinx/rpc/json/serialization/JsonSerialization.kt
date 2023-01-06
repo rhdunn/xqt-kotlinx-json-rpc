6b22af55ec7d1ed482c8997444007b438b79cfb2
Reece H. Dunn <msclrhd@gmail.com>
2023-01-06 14:03:08 +0000

    Add (de)serializeOrNull helpers to JsonSerialization.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/JsonSerialization.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/JsonSerialization.kt
@@ -1,7 +1,8 @@
-// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.rpc.json.serialization
 
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
 
 /**
  * Defines serialization for a data type or object to/from JSON elements.
@@ -13,7 +14,24 @@ interface JsonSerialization<T> {
     fun serializeToJson(value: T): JsonElement
 
     /**
+     * Serialize the nullable data type or object `value` to JSON, or null.
+     */
+    fun serializeToJsonOrNull(value: T?): JsonElement = when (value) {
+        null -> JsonNull
+        else -> serializeToJson(value)
+    }
+
+    /**
      * Deserialize the data type or object from the `json` element.
      */
     fun deserialize(json: JsonElement): T
+
+    /**
+     * Deserialize the nullable data type or object from the `json` element,
+     * or null.
+     */
+    fun deserializeOrNull(json: JsonElement): T? = when (json) {
+        JsonNull -> null
+        else -> deserialize(json)
+    }
 }
