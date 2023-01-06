436ad44625c87748dd60c9eacc2deebe1a590528
Reece H. Dunn <msclrhd@gmail.com>
2023-01-06 17:33:13 +0000

    Message

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/protocol/Message.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/protocol/Message.kt
@@ -1,12 +1,17 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.rpc.json.protocol
 
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import xqt.kotlinx.rpc.json.serialization.JsonSerialization
+import xqt.kotlinx.rpc.json.serialization.unsupportedKindType
+
 /**
  * A general message as defined by JSON-RPC.
  *
  * @see <a href="https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#abstractMessage">LSP 3.17 Abstract Message</a>
  */
-interface Message {
+sealed interface Message {
     /**
      * The version of the JSON-RPC protocol.
      *
@@ -14,10 +19,26 @@ interface Message {
      */
     val jsonprc: String
 
-    companion object {
+    companion object : JsonSerialization<Message> {
         /**
          * JSON-RPC 2.0
          */
         const val JSON_RPC_2_0 = "2.0"
+
+        override fun serializeToJson(value: Message): JsonElement = when (value) {
+            is RequestObject -> RequestObject.serializeToJson(value)
+            is ResponseObject -> ResponseObject.serializeToJson(value)
+            is Notification -> Notification.serializeToJson(value)
+        }
+
+        override fun deserialize(json: JsonElement): Message = when {
+            json !is JsonObject -> unsupportedKindType(json)
+            json.containsKey("method") -> when {
+                json.containsKey("id") -> RequestObject.deserialize(json)
+                else -> Notification.deserialize(json)
+            }
+
+            else -> ResponseObject.deserialize(json)
+        }
     }
 }
---
+++ src/xqt-kotlinx-json-rpc/commonTest/xqt/kotlinx/rpc/json/test/protocol/Message.kt
@@ -0,0 +1,175 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.test.protocol
+
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.rpc.json.protocol.*
+import xqt.kotlinx.rpc.json.serialization.UnsupportedKindTypeException
+import xqt.kotlinx.rpc.json.serialization.jsonArrayOf
+import xqt.kotlinx.rpc.json.serialization.jsonObjectOf
+import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
+import xqt.kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+@DisplayName("The Message type")
+class TheMessageType {
+    @Test
+    @DisplayName("supports notifications without parameters")
+    fun supports_notifications_without_parameters() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "method" to JsonPrimitive("test")
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(Notification::class, message::class)
+
+        val notification = message as Notification
+        assertEquals("2.0", notification.jsonprc)
+        assertEquals("test", notification.method)
+        assertEquals(null, notification.params)
+
+        assertEquals(json.toString(), Message.serializeToJson(notification).toString())
+    }
+
+    @Test
+    @DisplayName("supports notifications with parameters")
+    fun supports_notifications_with_parameters() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "method" to JsonPrimitive("test"),
+            "params" to jsonArrayOf(JsonPrimitive(1))
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(Notification::class, message::class)
+
+        val notification = message as Notification
+        assertEquals("2.0", notification.jsonprc)
+        assertEquals("test", notification.method)
+        assertEquals(jsonArrayOf(JsonPrimitive(1)), notification.params)
+
+        assertEquals(json.toString(), Message.serializeToJson(notification).toString())
+    }
+
+    @Test
+    @DisplayName("supports request objects without parameters")
+    fun supports_request_objects_without_parameters() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "method" to JsonPrimitive("test"),
+            "id" to JsonPrimitive(1234)
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(RequestObject::class, message::class)
+
+        val request = message as RequestObject
+        assertEquals("2.0", request.jsonprc)
+        assertEquals("test", request.method)
+        assertEquals(JsonIntOrString.IntegerValue(1234), request.id)
+        assertEquals(null, request.params)
+
+        assertEquals(json.toString(), Message.serializeToJson(request).toString())
+    }
+
+    @Test
+    @DisplayName("supports request objects with parameters")
+    fun supports_request_objects_with_parameters() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "method" to JsonPrimitive("test"),
+            "id" to JsonPrimitive(1234),
+            "params" to jsonArrayOf(JsonPrimitive(1))
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(RequestObject::class, message::class)
+
+        val request = message as RequestObject
+        assertEquals("2.0", request.jsonprc)
+        assertEquals("test", request.method)
+        assertEquals(JsonIntOrString.IntegerValue(1234), request.id)
+        assertEquals(jsonArrayOf(JsonPrimitive(1)), request.params)
+
+        assertEquals(json.toString(), Message.serializeToJson(request).toString())
+    }
+
+    @Test
+    @DisplayName("supports response object")
+    fun supports_response_objects() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "id" to JsonPrimitive(1234),
+            "result" to JsonPrimitive("lorem ipsum")
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(ResponseObject::class, message::class)
+
+        val response = message as ResponseObject
+        assertEquals("2.0", response.jsonprc)
+        assertEquals(JsonIntOrString.IntegerValue(1234), response.id)
+        assertEquals(JsonPrimitive("lorem ipsum"), response.result)
+        assertEquals(null, response.error)
+
+        assertEquals(json.toString(), Message.serializeToJson(response).toString())
+    }
+
+    @Test
+    @DisplayName("supports error response objects")
+    fun supports_error_response_object() {
+        val json = jsonObjectOf(
+            "jsonrpc" to JsonPrimitive("2.0"),
+            "id" to JsonPrimitive("abcdef"),
+            "error" to jsonObjectOf(
+                "code" to JsonPrimitive(-32601),
+                "message" to JsonPrimitive("Method 'foo' not found.")
+            )
+        )
+
+        val message = Message.deserialize(json)
+        assertEquals(ResponseObject::class, message::class)
+
+        val response = message as ResponseObject
+        assertEquals("2.0", response.jsonprc)
+        assertEquals(JsonIntOrString.StringValue("abcdef"), response.id)
+        assertEquals(null, response.result)
+
+        assertEquals(ErrorCode.MethodNotFound, response.error?.code)
+        assertEquals("Method 'foo' not found.", response.error?.message)
+        assertEquals(null, response.error?.data)
+
+        assertEquals(json.toString(), Message.serializeToJson(response).toString())
+    }
+
+    @Test
+    @DisplayName("throws an error if the kind type is not supported")
+    fun throws_an_error_if_the_kind_type_is_not_supported() {
+        val e1 = assertFails { Message.deserialize(jsonArrayOf()) }
+        assertEquals(UnsupportedKindTypeException::class, e1::class)
+        assertEquals("Unsupported kind type 'array'", e1.message)
+
+        val e2 = assertFails { Message.deserialize(JsonNull) }
+        assertEquals(UnsupportedKindTypeException::class, e2::class)
+        assertEquals("Unsupported kind type 'null'", e2.message)
+
+        val e3 = assertFails { Message.deserialize(JsonPrimitive("test")) }
+        assertEquals(UnsupportedKindTypeException::class, e3::class)
+        assertEquals("Unsupported kind type 'string'", e3.message)
+
+        val e4 = assertFails { Message.deserialize(JsonPrimitive(true)) }
+        assertEquals(UnsupportedKindTypeException::class, e4::class)
+        assertEquals("Unsupported kind type 'boolean'", e4.message)
+
+        val e5 = assertFails { Message.deserialize(JsonPrimitive(1)) }
+        assertEquals(UnsupportedKindTypeException::class, e5::class)
+        assertEquals("Unsupported kind type 'integer'", e5.message)
+
+        val e6 = assertFails { Message.deserialize(JsonPrimitive(1.2)) }
+        assertEquals(UnsupportedKindTypeException::class, e6::class)
+        assertEquals("Unsupported kind type 'decimal'", e6.message)
+    }
+}
