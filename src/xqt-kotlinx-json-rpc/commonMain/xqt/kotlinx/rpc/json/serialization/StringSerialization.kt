98bc213f2c07de272693062f7479b99d05f3aa5e
Reece H. Dunn <msclrhd@gmail.com>
2023-01-15 09:38:53 +0000

    Implement JsonSerialization on the StringSerialization interface.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/StringSerialization.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/StringSerialization.kt
@@ -1,10 +1,14 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.rpc.json.serialization
 
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+
 /**
  * Defines serialization for a data type or object to/from string values.
  */
-interface StringSerialization<T> {
+interface StringSerialization<T> : JsonSerialization<T> {
     /**
      * Serialize the data type or object `value` to a string.
      */
@@ -14,4 +18,8 @@ interface StringSerialization<T> {
      * Deserialize the data type or object from a string representation.
      */
     fun deserialize(value: String): T
+
+    override fun serializeToJson(value: T): JsonElement = JsonPrimitive(serializeToString(value))
+
+    override fun deserialize(json: JsonElement): T = deserialize(JsonString.deserialize(json))
 }
