3a50079b819defb8d354cc1aeb6c5168bf5cc88d
Reece H. Dunn <msclrhd@gmail.com>
2023-01-17 12:11:10 +0000

    Define a LineEnding enumeration.

---
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/io/LineEnding.kt
@@ -0,0 +1,27 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.io
+
+/**
+ * The line ending for different platforms.
+ *
+ * @param firstByte the first byte in the line ending
+ * @param secondByte the optional second byte in the line ending
+ */
+enum class LineEnding(val firstByte: Byte, val secondByte: Byte? = null) {
+    /**
+     * Line feed (LF) on Posix systems.
+     */
+    Lf('\n'.code.toByte()),
+
+    /**
+     * Carriage Return (CR) Line Feed (LF) on Windows systems.
+     *
+     * This is also used in HTTP and related protocols.
+     */
+    CrLf('\r'.code.toByte(), '\n'.code.toByte()),
+
+    /**
+     * Carriage Return (CR) on Mac systems.
+     */
+    Cr('\r'.code.toByte())
+}
