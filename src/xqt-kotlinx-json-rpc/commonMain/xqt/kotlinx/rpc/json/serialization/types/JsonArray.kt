65b6db8478d18c79da71364dcc4ce49148c32b18
Reece H. Dunn <msclrhd@gmail.com>
2023-02-13 07:49:02 +0000

    Add a JsonTypedArray helper serialization type.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/JsonArray.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/JsonArray.kt
@@ -1,8 +1,9 @@
-// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.rpc.json.serialization.types
 
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.buildJsonArray
 import xqt.kotlinx.rpc.json.serialization.JsonSerialization
 import xqt.kotlinx.rpc.json.serialization.unsupportedKindType
 
@@ -15,5 +16,21 @@ object JsonArray : JsonSerialization<JsonArray> {
     override fun deserialize(json: JsonElement): JsonArray = when (json) {
         is JsonArray -> json
         else -> unsupportedKindType(json)
+    }
+}
+
+/**
+ * Defines a typed JSON array.
+ *
+ * @param itemSerialization how to serialize the items in the array
+ */
+data class JsonTypedArray<T>(private val itemSerialization: JsonSerialization<T>) : JsonSerialization<List<T>> {
+    override fun serializeToJson(value: List<T>): JsonElement = buildJsonArray {
+        value.forEach { item -> add(itemSerialization.serializeToJson(item)) }
+    }
+
+    override fun deserialize(json: JsonElement): List<T> = when (json) {
+        !is JsonArray -> unsupportedKindType(json)
+        else -> json.map { item -> itemSerialization.deserialize(item) }
     }
 }
