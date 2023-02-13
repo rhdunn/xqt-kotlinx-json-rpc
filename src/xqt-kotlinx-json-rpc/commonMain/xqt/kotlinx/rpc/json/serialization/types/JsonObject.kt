fdc3bc18feb1f1b50be96c8b4eaff2f78eaa0b56
Reece H. Dunn <msclrhd@gmail.com>
2023-02-13 08:22:17 +0000

    Add a JsonTypedObject helper serialization type.

--- src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/JsonObject.kt
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/serialization/types/JsonObject.kt
@@ -1,9 +1,11 @@
-// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.rpc.json.serialization.types
 
+import kotlinx.serialization.json.*
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import xqt.kotlinx.rpc.json.serialization.JsonSerialization
+import xqt.kotlinx.rpc.json.serialization.StringSerialization
 import xqt.kotlinx.rpc.json.serialization.unsupportedKindType
 
 /**
@@ -15,5 +17,33 @@ object JsonObject : JsonSerialization<JsonObject> {
     override fun deserialize(json: JsonElement): JsonObject = when (json) {
         is JsonObject -> json
         else -> unsupportedKindType(json)
+    }
+}
+
+/**
+ * Defines a typed JSON object.
+ *
+ * @param keySerialization how to serialize the keys in the object map
+ * @param valueSerialization how to serialize the values in the object map
+ */
+data class JsonTypedObject<K, V>(
+    private val keySerialization: StringSerialization<K>,
+    private val valueSerialization: JsonSerialization<V>
+) : JsonSerialization<Map<K, V>> {
+    override fun serializeToJson(value: Map<K, V>): JsonElement = buildJsonObject {
+        value.forEach { (key, value) ->
+            val k = keySerialization.serializeToString(key)
+            val v = valueSerialization.serializeToJson(value)
+            put(k, v)
+        }
+    }
+
+    override fun deserialize(json: JsonElement): Map<K, V> = when (json) {
+        !is JsonObject -> unsupportedKindType(json)
+        else -> json.entries.associate { (key, value) ->
+            val k = keySerialization.deserialize(key)
+            val v = valueSerialization.deserialize(value)
+            k to v
+        }
     }
 }
