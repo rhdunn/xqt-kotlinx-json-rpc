914ff2b79c9bdac91fcc92800e7c2b96346b4d3d
Reece H. Dunn <msclrhd@gmail.com>
2023-05-20 09:17:40 +0100

    Uri

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
-- A URI `Authority` model class.
+- URI `Authority` and `Uri` model classes.
+- `InvalidHost` and `InvalidPortNumber` URI exceptions.
 
 ## [1.0.3] - 2023-05-22
 
---
+++ src/xqt-kotlinx-json-rpc/commonMain/xqt/kotlinx/rpc/json/uri/Uri.kt
@@ -0,0 +1,44 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.rpc.json.uri
+
+/**
+ * A Universal Resource Identifier (URI).
+ *
+ * @see <a href="https://www.rfc-editor.org/rfc/rfc3986">RFC 3986 Uniform Resource Identifier (URI): Generic Syntax</a>
+ */
+data class Uri(
+    /**
+     * The URI scheme.
+     *
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc3986#section-3.1">RFC 3986 (3.1) Scheme</a>
+     */
+    val scheme: String,
+
+    /**
+     * The URI authority.
+     *
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc3986#section-3.2">RFC 3986 (3.2) Authority</a>
+     */
+    val authority: Authority? = null,
+
+    /**
+     * The URI path.
+     *
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc3986#section-3.3">RFC 3986 (3.3) Path</a>
+     */
+    val path: String,
+
+    /**
+     * The URI query.
+     *
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc3986#section-3.4">RFC 3986 (3.4) Query</a>
+     */
+    val query: String? = null,
+
+    /**
+     * The URI fragment.
+     *
+     * @see <a href="https://www.rfc-editor.org/rfc/rfc3986#section-3.5">RFC 3986 (3.5) Fragment</a>
+     */
+    val fragment: String? = null
+)
